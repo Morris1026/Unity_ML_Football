7b4ca33c7179ef24e9bbb78eb9dff88b604661d1
Morris1026 <55180715+Morris1026@users.noreply.github.com>
2020-04-13 15:24:22 +0800

    Day.3

---
+++ Unity_ML_Football/Assets/Scripts/Ball.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Ball: MonoBehaviour
+{
+
+    public static bool complete;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "進球感應區")
+        {
+            complete = true;
+        }
+    }
+}
